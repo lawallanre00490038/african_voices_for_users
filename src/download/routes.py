2aa498dfa012695479a33d096341ae3ba3f4c248
Olanrewaju <lawallanre.abbahouse@gmail.com>
2025-07-29 12:08:27 +0100

    added estimate function

--- src/download/routes.py
+++ src/download/routes.py
@@ -5,26 +5,11 @@ from src.db.db import get_session
 from src.auth.utils import get_current_user
 from src.auth.schemas import TokenUser
 from src.download.service import DownloadService
-from src.download.schemas import AudioPreviewResponse
+from src.download.schemas import AudioPreviewResponse, EstimatedSizeResponse
 
 download_router = APIRouter()
 download_service = DownloadService()
 
-
-
-@download_router.get("/zip/{language}/{pct}", response_class=StreamingResponse)
-async def download_zip(
-    language: str,
-    pct: int,
-    background_tasks: BackgroundTasks,
-    as_excel: bool = True,
-    current_user: TokenUser = Depends(get_current_user),
-    session: AsyncSession = Depends(get_session),
-):
-    """
-    Download a zip file of audio datasets for a given language and percentage.
-    """
-    return await download_service.download_zip_with_metadata(language, pct, session, background_tasks, current_user, as_excel=as_excel)
 
 
 
@@ -51,3 +36,36 @@ async def preview_audio_samples(
     return await download_service.preview_audio_samples(
         session, language, limit, gender, age, education, domain
     )
+
+
+
+@download_router.get("/zip/estimate-size/{language}/{pct}", response_model=EstimatedSizeResponse)
+async def estimate_zip_size(
+    language: str,
+    pct: int,
+    current_user: TokenUser = Depends(get_current_user),
+    session: AsyncSession = Depends(get_session),
+):
+    return await download_service.estimate_zip_size_only(
+        language=language,
+        pct=pct,
+        session=session
+    )
+
+
+
+@download_router.get("/zip/{language}/{pct}", response_class=StreamingResponse)
+async def download_zip(
+    language: str,
+    pct: int,
+    background_tasks: BackgroundTasks,
+    as_excel: bool = True,
+    current_user: TokenUser = Depends(get_current_user),
+    session: AsyncSession = Depends(get_session),
+):
+    """
+    Download a zip file of audio datasets for a given language and percentage.
+    """
+    return await download_service.download_zip_with_metadata(language, pct, session, background_tasks, current_user, as_excel=as_excel)
+
+
