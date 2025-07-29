6be98ec90565e7695d574dba2579aafc2f3ff139
Olanrewaju <lawallanre.abbahouse@gmail.com>
2025-07-29 09:32:35 +0100

    reallign the datasets

--- src/download/s3_config.py
+++ src/download/s3_config.py
@@ -1,7 +1,6 @@
 import boto3
 from dotenv import load_dotenv
 from src.config import settings
-import os
 
 load_dotenv()
 
@@ -10,7 +9,7 @@ s3 = boto3.client(
     's3',
     aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
     aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-    region_name="eu-north-1",
+    region_name=settings.AWS_REGION,
     endpoint_url=settings.AWS_ENDPOINT_URL
 )
 
