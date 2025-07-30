1ecba0e80b6923fd683544088ba8dc9bea38d76d
Olanrewaju <lawallanre.abbahouse@gmail.com>
2025-07-30 04:08:40 +0100

    added some files

---
+++ upload_files_to_s3.py
@@ -0,0 +1,15 @@
+import os
+from src.download.s3_config import s3, BUCKET
+
+s3 = s3
+
+bucket_name = BUCKET
+local_directory = os.path.join(os.getcwd(), 'samples')
+s3_prefix = 'data'
+
+for root, _, files in os.walk(local_directory):
+    for file in files:
+        local_path = os.path.join(root, file)
+        s3_key = os.path.join(s3_prefix, os.path.relpath(local_path, local_directory)).replace('\\', '/')
+        s3.upload_file(local_path, bucket_name, s3_key)
+        print(f"Uploaded {local_path} to s3://{bucket_name}/{s3_key}")
